824f1c5a6120ed6a2e87e8fe121caca480ee9e61
nswa17 <nswa17@gmail.com>
2017-10-10 22:15:36 +0900

    Edit styles

--- src/controllers/styles.js
+++ src/controllers/styles.js
@@ -1,90 +1,74 @@
 module.exports = [
 	{
-		id: "ACADEMIC",
+		id: 1,
 		name: "Academic",
 		speaker_num_per_team: 4,
 		team_num: 2,
 		positions: ["Affirmative", "Negative"],
 		positions_short: ["Aff", "Neg"],
-		score_weights: [1, 1, 1, 1],
-		replies: [],
-		reply_num: 0
+		score_weights: [1, 1, 1, 1]
 	},
 	{
-		id: "NA",
+		id: 2,
 		name: "North American",
 		speaker_num_per_team: 2,
 		team_num: 2,
 		positions: ["Government", "Opposition"],
 		positions_short: ["Gov", "Opp"],
-		score_weights: [1, 1, 0.5],
-		replies: [1],
-		reply_num: 1
+		score_weights: [1, 1, 0.5]
 	},
 	{
-		id: "NAFA",
+		id: 3,
 		name: "NAFA",
 		speaker_num_per_team: 2,
 		team_num: 2,
 		positions: ["Affirmative", "Negative"],
 		positions_short: ["Aff", "Opp"],
-		score_weights: [1, 1, 1, 1],
-		replies: [],
-		reply_num: 0
+		score_weights: [1, 1, 1, 1]
 	},
 	{
-		id: "PDA3",
+		id: 4,
 		name: "PDA3",
 		speaker_num_per_team: 3,
 		team_num: 2,
 		positions: ["Government", "Opposition"],
 		positions_short: ["Gov", "Opp"],
-		score_weights: [1, 1, 1],
-		replies: [],
-		reply_num: 0
+		score_weights: [1, 1, 1]
 	},
 	{
-		id: "PDA4",
+		id: 5,
 		name: "PDA4",
 		speaker_num_per_team: 4,
 		team_num: 2,
 		positions: ["Government", "Opposition"],
 		positions_short: ["Gov", "Opp"],
-		score_weights: [1, 0.5, 0.5, 1],
-		replies: [],
-		reply_num: 0
+		score_weights: [1, 0.5, 0.5, 1]
 	},
 	{
-		id: "ASIAN",
+		id: 6,
 		name: "Asian",
 		speaker_num_per_team: 3,
 		team_num: 2,
 		positions: ["Government", "Opponents"],
 		positions_short: ["Gov", "Opp"],
-		score_weights: [1, 1, 1, 0.5],
-		replies: [0, 1],
-		reply_num: 1
+		score_weights: [1, 1, 1, 0.5]
 	},
 	{
-		id: "BP",
+		id: 7,
 		name: "British Parliamentary",
 		speaker_num_per_team: 2,
 		team_num: 4,
 		positions: ["Opening Government", "Opening Opposition", "Closing Government", "Closing Opposition"],
 		positions_short: ["OG", "OO", "CG", "CO"],
-		score_weights: [1, 1],
-		replies: [],
-		reply_num: 0
+		score_weights: [1, 1]
 	},
 	{
-		id: "PF",
+		id: 8,
 		name: "Public Forum",
 		speaker_num_per_team: 2,
 		team_num: 2,
 		positions: ["Affirmative", "Negative"],
 		positions_short: ["Aff", "Neg"],
-		score_weights: [1, 1, 1, 1],
-		replies: [0, 1],
-		reply_num: 2
+		score_weights: [1, 1, 1, 1]
 	}
 ]
